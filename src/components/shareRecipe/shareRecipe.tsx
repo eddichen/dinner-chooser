2d3abea0e237c5cbae16e7daa3d932737c00983e
eddichen <contacteddichen@gmail.com>
2020-07-06 16:11:14 +0100

    adding basic styling

--- src/components/shareRecipe/shareRecipe.tsx
+++ src/components/shareRecipe/shareRecipe.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Share, Facebook, Twitter } from 'react-feather';
 
 import useSiteMetadata from '../../hooks/use-site-metadata';
+
+const ShareList = styled.ul`
+  list-style-type: none;
+  font-family: ${props => props.theme.fontFamily.sansSerif};
+  margin: 0;
+  display: flex;
+`;
+
+const ShareListItem = styled.li`
+  margin-right: ${props => props.theme.spacing.md};
+  font-weight: bold;
+`;
+
+const ShareListLink = styled.a`
+  color: ${props => props.theme.color.black};
+`;
+
+const ShareButton = styled.button`
+  border: none;
+  background-color: transparent;
+  display: flex;
+  align-items: center;
+`;
+
+const ShareButtonText = styled.span`
+  margin-left: ${props => props.theme.spacing.sm};
+`;
 
 const ShareRecipe = ({ location, title }) => {
   const { siteURL } = useSiteMetadata();
@@ -17,18 +45,18 @@ const ShareRecipe = ({ location, title }) => {
   return (
     <>
       {
-        typeof window !== 'undefined' && navigator.share !== undefined ? <button onClick={() => shareButton()}><Share /> Share</button> : (
-          <ul>
-            <li>
+        typeof window !== 'undefined' && navigator.share !== undefined ? <ShareButton onClick={() => shareButton()}><Share /><ShareButtonText>Share</ShareButtonText></ShareButton> : (
+          <ShareList>
+            <ShareListItem>
               <span>Share to:</span>
-            </li>
-            <li>
-              <a href='https://twitter.com/share'><Twitter /></a>
-            </li>
-            <li>
-              <a href='https://www.facebook.com/sharer/sharer.php'><Facebook /></a>
-            </li>
-          </ul>
+            </ShareListItem>
+            <ShareListItem>
+              <ShareListLink href='https://twitter.com/share'><Twitter /></ShareListLink>
+            </ShareListItem>
+            <ShareListItem>
+              <ShareListLink href='https://www.facebook.com/sharer/sharer.php'><Facebook /></ShareListLink>
+            </ShareListItem>
+          </ShareList>
         )
       }
     </>
