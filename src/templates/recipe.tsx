0dc880bc8e33b455d6418db3d533c70c5452ccd2
eddichen <contacteddichen@gmail.com>
2020-07-08 10:40:50 +0100

    fixing ts errors in recipe and separating styles to a separate file

--- src/templates/recipe.tsx
+++ src/templates/recipe.tsx
@@ -1,120 +1,52 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { graphql } from 'gatsby';
-import styled from 'styled-components';
 import { Clock, Users } from 'react-feather';
 
 import Layout from '../components/layout';
 import useSiteMetadata from '../hooks/use-site-metadata';
 import ShareRecipe from '../components/shareRecipe/shareRecipe';
+import {
+  RecipeLayout,
+  RecipeAttributes,
+  RecipeAttribute,
+  RecipeAttributeText,
+  RecipeIntro,
+  RecipeImage,
+  RecipeTitle,
+  RecipeTitleSecondary,
+  Ingredients,
+  IngredientsList,
+  RecipeSteps,
+  RecipeStepsListItem,
+  Preparation,
+  Method
+} from './recipeStyles';
 
 interface Recipe {
-  data: {
-    contentfulRecipe: {
-      image: {
-        description: string;
-        file: {
-          url: string;
-        };
-      };
-      title: string;
+  contentfulRecipe: {
+    image: {
       description: string;
-      cookingTimeMins: number;
-      ingredients: [string];
-      preparation: [string];
-      method: [string];
-      serves: number;
+      file: {
+        url: string;
+      };
     };
+    title: string;
+    description: string;
+    cookingTimeMins: number;
+    ingredients: string[];
+    preparation: string[];
+    method: string[];
+    serves: number;
   };
 }
 
-const RecipeLayout = styled.div`
-  @media screen and (min-width: ${props => props.theme.breakpoint.md}) {
-    display: grid;
-    grid-template-columns: 2fr ${props => props.theme.spacing.xl} 1fr;
-  }
-`;
+interface RecipeInfo {
+  data: Recipe;
+  location: Location;
+}
 
-const RecipeAttributes = styled.div`
-  display: flex;
-  font-family: ${props => props.theme.fontFamily.sansSerif};
-`;
-
-const RecipeAttribute = styled.p`
-  display: flex;
-  margin-right: ${props => props.theme.spacing.md};
-  margin-bottom: 0;
-`;
-
-const RecipeAttributeText = styled.span`
-  display: inline-block;
-  margin-left: ${props => props.theme.spacing.sm};
-`;
-
-const RecipeIntro = styled.div`
-  margin-bottom: ${props => props.theme.spacing.lg};
-
-  @media screen and (min-width: ${props => props.theme.breakpoint.md}) {
-    grid-column: 1;
-    grid-row: 1;
-    align-self: end;
-  }
-`;
-
-const RecipeImage = styled.div`
-  margin-bottom: ${props => props.theme.spacing.lg};
-
-  @media screen and (min-width: ${props => props.theme.breakpoint.md}) {
-    grid-column: 3;
-    grid-row: 1;
-  }
-`;
-
-const RecipeTitle = styled.h1`
-  font-size: ${props => props.theme.fontSize.xxl};
-`;
-
-const RecipeTitleSecondary = styled.h2`
-  font-size: ${props => props.theme.fontSize.xl};
-  margin-bottom: ${props => props.theme.spacing.md};
-`;
-
-const Ingredients = styled.div`
-  font-size: ${props => props.theme.fontSize.lg};
-  margin-bottom: ${props => props.theme.spacing.lg};
-
-  @media screen and (min-width: 48em) {
-    grid-column: 3;
-  }
-`;
-
-const IngredientsList = styled.ul`
-  list-style-type: none;
-  margin: 0;
-`;
-
-const RecipeSteps = styled.div`
-  @media screen and (min-width: 48em) {
-    grid-column: 1;
-    grid-row: 2;
-  }
-`;
-
-const RecipeStepsListItem = styled.li`
-  margin-bottom: ${props => props.theme.spacing.md};
-`;
-
-const Preparation = styled.div`
-  font-size: ${props => props.theme.fontSize.lg};
-  margin-bottom: ${props => props.theme.spacing.lg};
-`;
-
-const Method = styled.div`
-  font-size: ${props => props.theme.fontSize.lg};
-  margin-bottom: ${props => props.theme.spacing.lg};
-`;
-
-const Recipe = ({ data, location }) => {
+const Recipe = ({ data, location }: RecipeInfo) => {
   const { siteURL } = useSiteMetadata();
   const pageTitle = `Dinner Chooser - ${data.contentfulRecipe.title}`;
 
